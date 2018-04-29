b93757770f076e97c0835acea5751a40ae6aba5f
Leonardo Riether <leonardoalves8217@gmail.com>
2018-04-29 01:06:12 -0300

    Kinda works but not really

--- js/main.js
+++ js/main.js
@@ -4,9 +4,7 @@ import { Vector } from "./vector.js";
 
 let thep5 = new p5(function (p) {
 
-  let wave = new Wave();
-  let particle = new Particle(Vector.is(0, p.windowHeight/2), 20, 1);
-  particle.v = Vector.is(50, -300);
+  let wave = new Wave(250, p, p.windowWidth*0.1);
   let fr = 40;
   let dt = 1.0/fr;
 
@@ -20,10 +18,20 @@ let thep5 = new p5(function (p) {
   gui.Register([
     {
       type: 'range',
-      label: 'Mass',
-      min: 0.5, max: 10, step: 0.5,
-      object: particle,
-      property: 'm'
+      label: 'k',
+      min: 120, max: 500, step: 10,
+      object: wave,
+      property: 'k'
+    },
+    {
+      type: 'button',
+      label: 'Reset',
+      action: () => {
+        for (let i = wave.particles.length-1; i--; ) {
+          wave.particles[i].pos.y = p.windowHeight/2;
+          wave.particles[i].v = Vector.zero;
+        }
+      }
     }
   ])
 
@@ -33,17 +41,15 @@ let thep5 = new p5(function (p) {
     p.background(232, 238, 242);
     p.fill(132, 191, 237);
     p.stroke(97, 154, 198);
+    p.strokeWeight(4);
   };
   
-
   p.draw = () => {
-    particle.F = Vector.is(0, 200*particle.m);
-    particle.updateDynamics(p, dt);
-    particle.updateKinematics(p, dt);
-
     p.clear();
     p.background(232, 238, 242);
-    particle.draw(p);
+    wave.particles[0].pos.y = p.mouseY;
+    wave.update(p, dt);
+    wave.draw(p);
   };
 });
 
--- js/wave.js
+++ js/wave.js
@@ -2,12 +2,11 @@ import { Vector } from "./vector";
 
 export class Particle {
 
-  constructor(pos, r, m) {
+  constructor(pos, m) {
     this.pos = pos;
     this.v = Vector.zero;
     this.a = Vector.zero;
     this.F = Vector.zero;
-    this.r = r;
     this._m = m;
     this.m_inv = 1.0/m;
   }
@@ -20,39 +19,97 @@ export class Particle {
     this.m_inv = 1.0/this._m;
   }
 
-  updateDynamics(p, dt) {
+  /**
+   * Updates the forces on the system based on the positions
+   */
+  // yl and yr are particle_to_the_left.y and particle_to_the_right.y, respectively
+  updateDynamics(p, dt, k, yl, yr) {
+    // Sets yl and yr to the same y as 'this' particle, so it
+    // doesn't generate any force
+    yl = yl === false ? this.pos.y : yl;
+    yr = yr === false ? this.pos.y : yr;
+
+    let dyl = yl - this.pos.y;
+    let dyr = yr - this.pos.y;
+
+    this.F = new Vector(0, dyl*k + dyr*k);
+
     this.a = Vector.Scale(this.F, this.m_inv);
-    this.F = Vector.zero; // Zera a força todo frame
+    this.F = Vector.zero; // Zeroes the forces
   }
 
+  // After updating all the forces, updates velocity and position of the particle
   updateKinematics(p, dt) {
+    // TODO: use Verlet Integration
+    // TODO: understand Verlet Integration
     this.v.plus(Vector.Scale(this.a, dt));
     this.pos.plus(Vector.Scale(this.v, dt));
 
-    if (this.pos.y >= p.windowHeight) {
-      this.pos.y = p.windowHeight;
-      this.v.y *= -0.8;
-    }
+    // if (this.pos.y + Particle.r >= p.windowHeight) {
+    //   this.pos.y = p.windowHeight - Particle.r;
+    //   this.v.y *= -0.8;
+    // }
   }
 
+  // P-chan!
   draw(pchan) {
-    pchan.arc(this.pos.x, this.pos.y, this.r, this.r, 0, pchan.TAU);
+    pchan.arc(this.pos.x, this.pos.y, Particle.r, Particle.r, 0, pchan.TAU);
   }
 };
 
+// Some constants...
+Particle.r = 20;
+Particle.m = 1;
+
 export class Wave {
 
-  constructor(p) {
-    
+  constructor(k, p, margin) {
+    this.k = k;
+
+    // Initializes all particles
+    this.particles = [];
+    let x = margin + Particle.r;
+    while (x + Particle.r < p.windowWidth - margin) {
+      this.particles.push(new Particle(Vector.is(x, p.windowHeight/2), Particle.m));
+      x += Particle.r;
+    }
   }
 
-  update() {
+  update(p, dt) {
+    // Dynamics update
+    this.particles[this.particles.length-1].updateDynamics(
+      p, dt, this.k, 
+      this.particles[this.particles.length-2].pos.y, 
+      false
+    );
+    for (let i = this.particles.length-2; i > 0; i--) {
+      this.particles[i].updateDynamics(
+        p, dt, this.k,
+        this.particles[i-1].pos.y,
+        this.particles[i+1].pos.y
+      );
+    }
+    this.particles[0].updateDynamics(p, dt, this.k, false, this.particles[1].pos.y);
 
+    // Kinematics update...
+    // ikr only3 lines
+    for (let i = this.particles.length-1; i--; ) {
+      this.particles[i].updateKinematics(p, dt);
+    }
   }
 
-  draw() {
-
+  draw(p) {
+    for (let i = this.particles.length-1; i--; ) {
+      if (i > 0) {// if not on the last particle
+        // WHERE DO WE DRAW THE LINE?
+        // here. here we draw the line
+        p.line(this.particles[i-1].pos.x, this.particles[i-1].pos.y,
+               this.particles[i].pos.x, this.particles[i].pos.y);
+      }
+      this.particles[i].draw(p);
+    }
   }
 }
+
 
 console.log("Wave.ts loaded");
